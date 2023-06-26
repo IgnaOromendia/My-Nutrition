6c0eaffa5700972df1c531d9c7a58960f572f108
Ignacio Fernandez Oromendia <ignaciooromendia@gmail.com>
2023-06-26 19:58:41 -0300

    Global file add

---
+++ My Nutrition/Utility/Global.swift
@@ -0,0 +1,12 @@
+//
+//  Global.swift
+//  My Nutrition
+//
+//  Created by Igna on 26/06/2023.
+//
+
+import Foundation
+
+// Variables
+var current_week: Week = []     // This array has lenght = 7 always
+var weeks: [String:Week] = [:]
